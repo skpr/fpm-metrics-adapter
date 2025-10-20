ac68ceae60ec2ec87d1de155130128bad007a7c8
Nathan ter Bogt <nterbogt@gmail.com>
2025-10-20 21:12:51 +1300

    Simplifying the server. No logger. No ticker.

--- internal/sidecar/handler.go
+++ internal/sidecar/handler.go
@@ -3,22 +3,40 @@ package sidecar
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/patrickmn/go-cache"
+	"github.com/skpr/fpm-metrics-adapter/internal/fpm"
 )
 
 // Handler to return the latest status.
 func (s *Server) handler(w http.ResponseWriter, _ *http.Request) {
 	s.logger.Debug("Handling request")
 
-	jsonBytes, err := json.Marshal(s.status)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		s.logger.Error("failed to marshal status response", "error", err.Error())
-		return
+	var jsonBytes []byte
+	c, found := s.cache.Get("json")
+	if found {
+		s.logger.Debug("Cache hit")
+		jsonBytes = c.([]byte)
+	} else {
+		s.logger.Debug("Cache miss")
+		status, err := fpm.QueryStatus(s.config.Endpoint)
+		if err != nil {
+			s.logger.Error("Error querying fpm", "error", err.Error())
+		}
+
+		jsonBytes, err = json.Marshal(status)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			s.logger.Error("failed to marshal status response", "error", err.Error())
+			return
+		}
+
+		s.cache.Set("json", jsonBytes, cache.DefaultExpiration)
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(jsonBytes)
+	_, err := w.Write(jsonBytes)
 	if err != nil {
 		s.logger.Error("failed to write status response", "error", err.Error())
 		return
--- internal/sidecar/sidecar.go
+++ internal/sidecar/sidecar.go
@@ -7,21 +7,17 @@ import (
 	"net/http"
 	"time"
 
-	"golang.org/x/sync/errgroup"
-
-	"github.com/skpr/fpm-metrics-adapter/internal/fpm"
+	"github.com/patrickmn/go-cache"
 )
 
 // Server for collecting and returning
 type Server struct {
 	// Used for logging events.
 	logger *slog.Logger
-	// The most recently collected FPM status.
-	status fpm.Status
 	// Configuration used by the HTTP server.
 	config ServerConfig
-	// Configuration used by the status logger.
-	statusLogger LogStatus
+	// Cache for flood control.
+	cache *cache.Cache
 }
 
 // ServerConfig which is used by the HTTP server.
@@ -32,24 +28,14 @@ type ServerConfig struct {
 	Path string
 	// Endpoint for querying the latest FPM status information.
 	Endpoint string
-	// EndpointPoll frequency for collecting FPM status information.
-	EndpointPoll time.Duration
-}
-
-// LogStatus is used to configure the status logger.
-type LogStatus struct {
-	// If the status logger is enabled.
-	Enabled bool
-	// How frequency to log the status.
-	Frequency time.Duration
 }
 
 // NewServer for collecting and responding with the latest FPM status.
-func NewServer(logger *slog.Logger, config ServerConfig, statusLogger LogStatus) (*Server, error) {
+func NewServer(logger *slog.Logger, config ServerConfig) (*Server, error) {
 	server := &Server{
-		logger:       logger,
-		config:       config,
-		statusLogger: statusLogger,
+		logger: logger,
+		config: config,
+		cache:  cache.New(1*time.Second, 10*time.Second),
 	}
 
 	return server, nil
@@ -57,10 +43,6 @@ func NewServer(logger *slog.Logger, config ServerConfig, statusLogger LogStatus)
 
 // Run the HTTP server.
 func (s *Server) Run(ctx context.Context) error {
-	ctx, cancel := context.WithCancel(ctx)
-
-	group, ctx := errgroup.WithContext(ctx)
-
 	router := http.NewServeMux()
 
 	router.HandleFunc(s.config.Path, s.handler)
@@ -70,49 +52,14 @@ func (s *Server) Run(ctx context.Context) error {
 		Handler: router,
 	}
 
-	// Run the HTTP server.
-	group.Go(func() error {
-		// We want to shutdown all other tasks if this logger exits.
-		defer cancel()
+	s.logger.Info("Starting server")
 
-		s.logger.Info("Starting server")
+	err := srv.ListenAndServe()
+	if err != nil {
+		return err
+	}
 
-		err := srv.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			return err
-		}
+	s.logger.Info("Shutting down server")
 
-		return nil
-	})
-
-	// Run the HTTP server.
-	group.Go(func() error {
-		<-ctx.Done()
-		s.logger.Info("Shutting down server")
-		return srv.Shutdown(ctx)
-	})
-
-	// Query FPM periodically for statistics.
-	group.Go(func() error {
-		// We want to shutdown all other tasks if this logger exits.
-		defer cancel()
-
-		s.logger.Info("Starting refresher")
-		return s.refreshStatus(ctx)
-	})
-
-	// Logger for emmit metrics as a log event for external systems.
-	group.Go(func() error {
-		if !s.statusLogger.Enabled {
-			return nil
-		}
-
-		// We want to shutdown all other tasks if this logger exits.
-		defer cancel()
-
-		s.logger.Info("Starting logger")
-		return s.logStatus(ctx)
-	})
-
-	return group.Wait()
+	return nil
 }
